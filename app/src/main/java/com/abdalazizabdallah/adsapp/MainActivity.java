06f7fb02b3d49d8b4df7ce4c85d1fbd7aca7b4bf
AbdAlAziz2000 <abdalazizabdalluh3@gmail.com>
2021-02-18 01:53:49 +0200

    create sign up fragment and first_fregment_after_splash fragment

--- app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
+++ app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
@@ -1,15 +1,27 @@
 package com.abdalazizabdallah.adsapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        String languageToLoad = "ar"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+
+        setContentView(R.layout.login_fragment);
 
 
     }
