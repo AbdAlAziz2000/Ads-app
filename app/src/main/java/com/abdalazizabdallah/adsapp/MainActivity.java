30da6e389f35b200d42ff65b95e30a72235b44ce
AbdAlAziz2000 <abdalazizabdalluh3@gmail.com>
2021-02-22 21:20:16 +0200

    update previous fragment and update post ads fragment

--- app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
+++ app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.abdalazizabdallah.adsapp;
 
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
                 getBaseContext().getResources().getDisplayMetrics());
 
         setContentView(R.layout.post_ads_fragment);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+
+        floatingActionButton = findViewById(R.id.fab_button);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            floatingActionButton.setBackground(getResources().getDrawable(R.drawable.bg_layer_button, null));
+        }
 
 
     }
