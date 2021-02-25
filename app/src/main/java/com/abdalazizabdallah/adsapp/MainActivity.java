b47aedab2717e91cdf87865e93ca1b323e48f7f8
AbdAlAziz2000 <abdalazizabdalluh3@gmail.com>
2021-02-26 00:53:43 +0200

    create three fragment update_account_fragment.xml , update_ads_fragment.xml , update_password_fragment.xml

--- app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
+++ app/src/main/java/com/abdalazizabdallah/adsapp/MainActivity.java
@@ -2,15 +2,14 @@ package com.abdalazizabdallah.adsapp;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -21,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton floatingActionButton;
     List<MyNotification> myNotificationList;
 
+    AutoCompleteTextView autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(config,
                 getBaseContext().getResources().getDisplayMetrics());
 
-        DataBindingUtil.setContentView(this, R.layout.notifications_fragment);
+        DataBindingUtil.setContentView(this, R.layout.update_ads_fragment);
+//
+//        String [] listOption = {"Option 1", "Option 2", "Option 3", "Option 4"};
+//        autoCompleteTextView = findViewById(R.id.post_ads_fragment_autoCompleteTextView);
+//        autoCompleteTextView.setAdapter(new ArrayAdapter<>(this, R.layout.auto_item,listOption));
 
 //        Ads ads = new Ads(getString(R.string.time_text), getString(R.string.ads_headline_text),
 //                String.valueOf(R.drawable.ipad2), getString(R.string.price_text_with_suffix, 300.001));
@@ -62,49 +66,48 @@ public class MainActivity extends AppCompatActivity {
 //
 //
 
-
-        myNotificationList = new ArrayList<>();
-
-
-        MyNotification myNotification = new MyNotification(getString(R.string.title_item_notification), getString(R.string.body_item_notification));
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-        myNotificationList.add(myNotification);
-
-        RecyclerView recyclerView = findViewById(R.id.notification_fragment_recycleView);
-        recyclerView.setAdapter(new NotificationListRecycleViewAdapter(myNotificationList));
+//        myNotificationList = new ArrayList<>();
+//
+//
+//        MyNotification myNotification = new MyNotification(getString(R.string.title_item_notification), getString(R.string.body_item_notification));
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//        myNotificationList.add(myNotification);
+//
+//        RecyclerView recyclerView = findViewById(R.id.notification_fragment_recycleView);
+//        recyclerView.setAdapter(new NotificationListRecycleViewAdapter(myNotificationList));
 
 //
 //
